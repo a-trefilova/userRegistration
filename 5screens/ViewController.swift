e6323771e03e37ae4c2e0f3dce1a3062bb6b4286
a-trefilova <atrefilova97@icloud.com>
2020-09-28 16:43:04 +0300

    Initial Commit

---
+++ 5screens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  5screens
+//
+//  Created by Константин Сабицкий on 28.09.2020.
+//  Copyright © 2020 Константин Сабицкий. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
