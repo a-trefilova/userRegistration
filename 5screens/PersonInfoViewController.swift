c3ec9b39a873facc46bfeb607a2589bc8a9f7cb1
a-trefilova <atrefilova97@icloud.com>
2020-10-05 22:09:09 +0300

    prototypes

---
+++ 5screens/PersonInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PersonInfoViewController.swift
+//  5screens
+//
+//  Created by Константин Сабицкий on 02.10.2020.
+//  Copyright © 2020 Константин Сабицкий. All rights reserved.
+//
+
+import UIKit
+
+class PersonInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
